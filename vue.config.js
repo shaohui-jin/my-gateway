9f08227b0f96dfb97a39f3a5b7b0e65a605b956e
shaohui_jin <1051131737@qq.com>
2023-05-17 10:04:37 +0800

    feature(调整): 调整
    移除terser-webpack-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-const TerserPlugin = require('terser-webpack-plugin');
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
@@ -10,22 +9,5 @@ module.exports = defineConfig({
     config.plugins.delete('preload');
     // 移除 prefetch(预取) 插件
     config.plugins.delete('prefetch');
-    // 生产环境下清除 console.log
-    if (process.env.NODE_ENV === 'production') {
-      return {
-        optimization: {
-          minimizer: [
-            new TerserPlugin({
-              sourceMap: false,
-              terserOptions: {
-                compress: {
-                  drop_console: true,
-                },
-              },
-            }),
-          ],
-        },
-      };
-    }
   },
 });
